e7578dd1c228ce5f6e04ff47c50c43a663c4fffc
owlcookies <kimsw2007@gmail.com>
2025-07-28 15:33:40 +0700

    git push origin main

--- CaesarCipher/cc_OOP.py
+++ CaesarCipher/cc_OOP.py
@@ -4,26 +4,36 @@ from tkinter import ttk
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.title("Apple")
+        self.geometry("1200x800")
         
-        #Variables for program
+        
+        self.set_variable()
+        self.set_frame()
+        self.set_texts()
+        self.set_textbox()
+        self.set_buttons()
+        
+        
+    def set_variable(self):
         self.alphabet = list("abcdefghijklmnopqrstuvwxyz")
         self.set_alphabet = set(self.alphabet)
         self.mode = ["encode","decode"]
         self.shift_number = tk.IntVar(self, 7)
-        self.current_mode = tk.StringVar(self, self.mode[0])
+        self.current_mode = tk.StringVar(self, self.mode[0])    
         
-        self.title("Apple")
-        self.geometry("1200x800")
-        
+    def set_frame(self):
         self.frm = ttk.Frame(self, padding=(50, 100), border=2)
         self.frm.grid()
         
-        #Text on screen 
+    def set_texts(self):
         self.program_title = ttk.Label(self.frm, text="Caesar Cipher", font=("Comic Sans MS",30)).grid(column=7, row=0)
         self.input_title = ttk.Label(self.frm, text="Input Text",font=("Comic Sans MS", 15)).grid(column=0, row=0,pady=30)
-        self.num_on_screen = ttk.Label(self.frm, textvariable=self.shift_number,font=("Comif Sans MS", 20)).grid(column=7,row=1, pady=20)
+        self.num_on_screen = ttk.Label(self.frm, textvariable=self.shift_number,font=("Comic Sans MS", 20)).grid(column=7,row=1, pady=20)
         self.output_title = ttk.Label(self.frm, text="Output Text", font=("Comic Sans MS", 15)).grid(column=9, row=0,padx=20,pady=30)
         
+        
+    def set_textbox(self):
         #Textbox(input)
         self.text_box = tk.Text(self.frm, width=40, height=20, relief="sunken")
         self.text_box.grid(column=0,row=20,padx=20)
@@ -32,15 +42,14 @@ class App(tk.Tk):
         self.display_box = tk.Text(self.frm, width=40, height=20, relief="sunken",state="disabled")
         self.display_box.grid(column=9,row=20,padx=20,pady=20)
         
+    def set_buttons(self):
         #Buttons
         self.change_mode = ttk.Button(self.frm, textvariable=self.current_mode, command=self.mode_change).grid(column=7,row=2)
         self.shift_num_up = ttk.Button(self.frm, text="+", command=self.shift_up).grid(column=6,row=1)
         self.shift_num_down = ttk.Button(self.frm, text="-", command=self.shift_down).grid(column=8,row=1)
         self.activate_button = ttk.Button(self.frm, text="begin", command=self.encode_decode)
-        self.activate_button.grid(column=7,row=7,pady=25)
-        
-        
-        
+        self.activate_button.grid(column=7,row=7,pady=25)    
+    
     def shift_up(self):    
         self.shift_number.set(self.shift_number.get() + 1)
         
